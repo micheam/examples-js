0d0060548ce7074adec84590dad15eada539764f
Michito Maeda <michito.maeda@gmail.com>
2019-06-24 19:02:21 +0900

    add sample feature

---
+++ hello-vuejs-world/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <ul>
+      <li 
+        v-for="n in 3" 
+        :key="n"
+        >
+        {{ n }}: Hello, {{ bundler }} !!
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  color: green;
+}
+</style>
